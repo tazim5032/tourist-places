bf2f2af3e240fed41dcab27f56da99e1cfc3b6da
tazim5032 <51076835+tazim5032@users.noreply.github.com>
2024-02-24 19:27:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function addPlace(){
+    const add = document.getElementById('list');
+
+    const div = document.createElement('div');
+
+    const id = document.createElement('div');
+    const idH = document.createElement('h1');
+    
+    const name = document.createElement('div');
+    const address = document.createElement('div');
+    const rating = document.createElement('div');
+    const pic = document.createElement('div');
+    const newBtn = document.createElement('div');
+}
